a9f7806d3ffd64e17099e755dec71c2c6fbcce11
Neil Patel <neilpatel235@gmail.com>
2024-04-22 05:37:11 -0400

    Insertion Sort

---
+++ src/routes/sorts/c_InsertionSort.js
@@ -0,0 +1,18 @@
+// insertion sort algorithm
+
+// data is a 1D array that holds objects as elements.
+
+// this will return an array from greatest to least score!!!
+function insertionSort(data) {
+    for (let i = 1; i < data.length; i++) {
+        let key = data[i];
+        let j = i - 1;
+
+        while (key.score > data[j].score && j >= 0) {
+            data[j + 1] = data[j];
+            j -= 1;
+        }
+        data[j + 1] = key;
+    }
+}
+
