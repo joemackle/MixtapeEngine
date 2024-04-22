096d1cfddb1336cf85bde2c7c8d6a4debde04490
Neil Patel <neilpatel235@gmail.com>
2024-04-22 16:49:27 -0400

    variable fix selection sort

--- src/lib/sorts/a_SelectionSort.js
+++ src/lib/sorts/a_SelectionSort.js
@@ -4,18 +4,18 @@
 
 // this will return an array from greatest to least score!!!
 function selectionSort(data) {
-  for (let i = 0; i < data.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < data.length; j++) {
-      // the condiction in the inner loop is in descending order
-      if (data[j].score > data[minIndex].score) {
-        minIndex = j;
-      }
+    for (let i = 0; i < data.length - 1; i++) {
+        let maxIndex = i;
+        for (let j = i + 1; j < data.length; j++) {
+            // the condiction in the inner loop is in descending order
+            if (data[j].score > data[maxIndex].score) {
+                maxIndex = j;
+            }
+        }
+        let temp = data[maxIndex];
+        data[maxIndex] = data[i];
+        data[i] = temp;
     }
-    let temp = data[minIndex];
-    data[minIndex] = data[i];
-    data[i] = temp;
-  }
 }
 
 export { selectionSort };
--- src/lib/sorts/f_QuickSort.js
+++ src/lib/sorts/f_QuickSort.js
@@ -1,1 +1,22 @@
 // quick sort algorithm
+
+// call this function like this...
+// quickSort(data, 0, data.length-1);
+function quickSort(arr, low, high) {
+    if (low < high) {
+        let pivot = partition(arr, low, high);
+        quickSort(arr, low, (pivot - 1));
+        quickSort(arr, (pivot + 1), high);
+    }
+}
+
+function partition(arr, low, high) {
+    // here we are selecting a pivot at random in the 114,000 length data array.
+    /*  
+        "Choosing a random pivot minimizes the chance that you will 
+        encounter worst-case O(n2) performance (always choosing first or last 
+        would cause worst-case performance for nearly-sorted 
+        or nearly-reverse-sorted data)" -- Stack Overflow
+    */
+    let pivot = arr[low];
+}
