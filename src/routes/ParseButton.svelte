d8ecfa7447e3c8efe58117584ceb11121ce76822
Neil Patel <neilpatel235@gmail.com>
2024-04-21 21:34:27 -0400

    created and adding 3 files

--- src/routes/ParseButton.svelte
+++ src/routes/ParseButton.svelte
@@ -1,15 +1,19 @@
 <script>
-  import {parseCsv} from './parseCsv.js';
-  export let weights;
-  export let result;
+	import { parseCsv } from './parseCsv.js';
+	export let weights;
+	export let result;
 
-  async function handleParse() {
-    const url = "/dataset.csv";
-    result = await parseCsv(url, weights);
-    console.log(result);
-  }
+	async function handleParse() {
+		const url = '/dataset.csv';
+		result = await parseCsv(url, weights);
+		console.log(result);
+	}
 </script>
 
-<button on:click={handleParse} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-  Parse CSV
+<button
+	on:click={handleParse}
+	type="button"
+	class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+>
+	Parse CSV
 </button>
